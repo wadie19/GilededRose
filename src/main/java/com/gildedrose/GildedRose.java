d4701d5e452ebc650b88f06794e8e315f1992476
wadie19 <rezgui.m.w40@gmail.com>
2023-10-14 16:11:09 +0200

    attributs ajouté

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,9 @@ package com.gildedrose;
 
 class GildedRose {
   Item[] items;
+  static final String sulfuras = "Sulfuras, Hand of Ragnaros";
+  static final String backstage = "Backstage passes to a TAFKAL80ETC concert";
+  static final String agedbrie = "Aged Brie";
 
   public GildedRose(Item[] items) {
     this.items = items;
@@ -9,10 +12,10 @@ class GildedRose {
 
   public void updateQuality() {
     for (int i = 0; i < items.length; i++) {
-      if (!items[i].name.equals("Aged Brie")
-          && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      if (!items[i].name.equals(agedbrie)
+          && !items[i].name.equals(backstage)) {
         if (items[i].quality > 0) {
-          if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+          if (!items[i].name.equals(sulfuras)) {
             items[i].quality = items[i].quality - 1;
           }
         }
@@ -20,7 +23,7 @@ class GildedRose {
         if (items[i].quality < 50) {
           items[i].quality = items[i].quality + 1;
 
-          if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+          if (items[i].name.equals(backstage)) {
             if (items[i].sellIn < 11) {
               if (items[i].quality < 50) {
                 items[i].quality = items[i].quality + 1;
@@ -36,15 +39,15 @@ class GildedRose {
         }
       }
 
-      if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+      if (!items[i].name.equals(sulfuras)) {
         items[i].sellIn = items[i].sellIn - 1;
       }
 
       if (items[i].sellIn < 0) {
-        if (!items[i].name.equals("Aged Brie")) {
-          if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (!items[i].name.equals(agedbrie)) {
+          if (!items[i].name.equals(backstage)) {
             if (items[i].quality > 0) {
-              if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+              if (!items[i].name.equals(sulfuras)) {
                 items[i].quality = items[i].quality - 1;
               }
             }
